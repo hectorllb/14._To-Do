70a23ba38d200256f13e924beae81e3aac91e03b
Héctor de la Llave <166048014+hectorllb@users.noreply.github.com>
2025-05-23 09:06:58 +0200

    Añadiendo selectores de ids en js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Selectores
+const bgColor = document.getElementById("bgColor");
+const fontSize = document.getElementById("fontSize");
+const themeToggle = document.getElementById("themeToggle");
+const defaultPriority = document.getElementById("defaultPriority");
+const defaultDueDate = document.getElementById("defaultDueDate");
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskPriority = document.getElementById("taskPriority");
+const taskDueDate = document.getElementById("taskDueDate");
+const taskTags = document.getElementById("taskTags");
+const taskList = document.getElementById("taskList");
+
+// Variables
+
+// Funciones
+
+// Eventos
